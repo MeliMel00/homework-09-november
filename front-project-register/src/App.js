98cb1fdbc6c5d54ee8a27ede6495aa38ebc2074b
Mel <melvin.chevallier@edu.devinci.fr>
2022-11-30 16:44:36 +0000

    first push

---
+++ front-project-register/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Registration from './component/Registration';
+function App() {
+  return (
+    <div className="App">
+    
+    <Registration></Registration>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-project-register/src/component/Registration.js
@@ -0,0 +1,75 @@
+import React, {useState,setState} from 'react';
+import Header from './Header';
+function Registration(){
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [confirmPassword,setConfirmPassword] = useState('');
+
+    const handleInputChange = (e) => {
+        const {id , value} = e.target;
+
+        
+        if(id === "firstName"){
+            setFirstName(value);
+        }
+        if(id === "lastName"){
+            setLastName(value);
+        }
+        if(id === "email"){
+            setEmail(value);
+        }
+        if(id === "password"){
+            setPassword(value);
+        }
+        if(id === "confirmPassword"){
+            setConfirmPassword(value);
+        }
+
+    }
+
+    const handleSubmit  = () => {
+        if (firstName === '' || lastName === '' || email === '' || password === '' || confirmPassword === '') {
+            alert('Please fill all the fields');
+        }else if(password !== confirmPassword){
+            alert('Passwords do not match');
+        }else {
+            alert('Registration successful');
+        }
+    }
+
+    return(
+        <div className="form">
+        <Header></Header>
+            <div className="form-body">
+                <div className="username div_input">
+                    <label className="form__label" htmlFor="firstName">First Name </label>
+                    <input className="form__input" type="text" value={firstName} onChange = {(e) => handleInputChange(e)} id="firstName" placeholder="First Name"/>
+                </div>
+                <div className="lastname div_input">
+                    <label className="form__label" htmlFor="lastName">Last Name </label>
+                    <input  type="text" name="" id="lastName" value={lastName}  className="form__input" onChange = {(e) => handleInputChange(e)} placeholder="LastName"/>
+                </div>
+                <div className="email div_input">
+                    <label className="form__label" htmlFor="email">Email </label>
+                    <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
+                </div>
+                <div className="password div_input">
+                    <label className="form__label" htmlFor="password">Password </label>
+                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
+                </div>
+                <div className="confirm-password div_input">
+                    <label className="form__label" htmlFor="confirmPassword">Confirm Password </label>
+                    <input className="form__input" type="password" id="confirmPassword" value={confirmPassword} onChange = {(e) => handleInputChange(e)} placeholder="Confirm Password"/>
+                </div>
+            </div>
+            <div className="footer">
+                <button onClick={()=>handleSubmit()} type="submit" className="btn">Register</button>
+            </div>
+        </div>
+    )
+}
+
+export default Registration
